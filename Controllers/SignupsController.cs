d221ce6718a79098d29bd4bbe87e0048470495d4
Priyank Panchal <prinkpan@gmail.com>
2022-02-20 12:32:14 -0500

    Added unit tests for .net API

--- Controllers/SignupsController.cs
+++ Controllers/SignupsController.cs
@@ -3,7 +3,6 @@ using ActivitySignup.Models;
 using ActivitySignup.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace ActivitySignup.Controllers
